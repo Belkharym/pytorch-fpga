39ae9638334bfa34997d2f91addb3ad4cee660b5
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2019-10-08 10:38:16 -0400

    opencl: Added eq.Scalar, ne.Tensor and ne.Scalar

--- aten/src/ATen/native/opencl/TensorFactories.cl
+++ aten/src/ATen/native/opencl/TensorFactories.cl
@@ -1,40 +1,52 @@
 #include "aten/src/ATen/native/opencl/OpenCLOperations.h"
 
-__kernel void pointwise_op_3f(__global const float* a, __global const float* b, __global float* c, enum OpenCLOperations op) {
-    // TODO See aten/src/THC/THCApply.cuh in the function THC_pointwiseApply3 for implementation details.
-    // The type 'Operations' is an enumaration, since we can't give a function pointer as a parameter
-    // in some implementations (for instance, on FPGAs). Thus we use predefined operations and we choose
-    // the operation within the enumaration (maybe using a switch-case statement).
+static float comp(const float a, const float b, enum OpenCLOperationsPointwise3 op) {
     switch(op) {
         case EQ:
-            c[get_global_id(0)] = a[get_global_id(0)] == b[get_global_id(0)];
+            return a == b;
             break;
         case NE:
-            c[get_global_id(0)] = a[get_global_id(0)] != b[get_global_id(0)];
+            return a != b;
             break;
         case GT:
-            c[get_global_id(0)] = a[get_global_id(0)] > b[get_global_id(0)];
+            return a > b;
             break;
         case LT:
-            c[get_global_id(0)] = a[get_global_id(0)] < b[get_global_id(0)];
+            return a < b;
             break;
         case GE:
-            c[get_global_id(0)] = a[get_global_id(0)] >= b[get_global_id(0)];
+            return a >= b;
             break;
         case LE:
-            c[get_global_id(0)] = a[get_global_id(0)] <= b[get_global_id(0)];
+            return a <= b;
             break;
     }
 }
 
-__kernel void pointwise_op_f(__global const float* a, __global float* c, enum OpenCLOperations op) {
+__kernel void pointwise_op_3f(__global const float* a, __global const float* b, __global float* c, enum OpenCLOperationsPointwise3 op) {
+    // TODO See aten/src/THC/THCApply.cuh in the function THC_pointwiseApply3 for implementation details.
+    // The type 'Operations' is an enumaration, since we can't give a function pointer as a parameter
+    // in some implementations (for instance, on FPGAs). Thus we use predefined operations and we choose
+    // the operation within the enumaration (maybe using a switch-case statement).
+    c[get_global_id(0)] = comp(a[get_global_id(0)], b[get_global_id(0)], op);
+}
+
+__kernel void pointwise_op_2fv(__global const float* a, const float b, __global float* c, enum OpenCLOperationsPointwise3 op) {
+    // TODO See aten/src/THC/THCApply.cuh in the function THC_pointwiseApply3 for implementation details.
+    // The type 'Operations' is an enumaration, since we can't give a function pointer as a parameter
+    // in some implementations (for instance, on FPGAs). Thus we use predefined operations and we choose
+    // the operation within the enumaration (maybe using a switch-case statement).
+    c[get_global_id(0)] = comp(a[get_global_id(0)], b, op);
+}
+
+__kernel void pointwise_op_f(__global const float* a, __global float* b, enum OpenCLOperationsPointwise op) {
     // TODO See aten/src/THC/THCApply.cuh in the function THC_pointwiseApply3 for implementation details.
     // The type 'Operations' is an enumaration, since we can't give a function pointer as a parameter
     // in some implementations (for instance, on FPGAs). Thus we use predefined operations and we choose
     // the operation within the enumaration (maybe using a switch-case statement).
     switch(op) {
         case ABS:
-            b[get_global_id(0)] = abs(a[get_global_id(0)]);
+            b[get_global_id(0)] = a[get_global_id(0)] < 0.0f ? -a[get_global_id(0)] : a[get_global_id(0)];
             break;
     }
 }
--- aten/src/ATen/native/opencl/TensorFactories.cpp
+++ aten/src/ATen/native/opencl/TensorFactories.cpp
@@ -40,7 +40,7 @@ Tensor empty_opencl(IntArrayRef size, const TensorOptions& options, c10::optiona
   return tensor;
 }
 
-static void pointwise_op3(TensorImpl* c, const TensorImpl* a, const TensorImpl* b, at::native::opencl::OpenCLOperations op) {
+static void pointwise_op3(StorageImpl* c, const StorageImpl* a, const StorageImpl* b, at::native::opencl::OpenCLOperationsPointwise3 op) {
   // DONE Call OpenCL kernel.
   auto opt_kernel = c10::opencl::opencl_kernel("pointwise_op_3f");
   if (!opt_kernel.has_value()) {
@@ -48,16 +48,35 @@ static void pointwise_op3(TensorImpl* c, const TensorImpl* a, const TensorImpl* 
     return;
   }
   cl::Kernel pointwise_op_f = opt_kernel.value();
-  pointwise_op_f.setArg<cl_mem>(0, (*(cl::Buffer*)a->storage().unsafeGetStorageImpl()->data_ptr().get())());pointwise_op_f.setArg<cl_mem>(1, (*(cl::Buffer*)b->storage().unsafeGetStorageImpl()->data_ptr().get())());
-  pointwise_op_f.setArg<cl_mem>(2, (*(cl::Buffer*)c->storage().unsafeGetStorageImpl()->data_ptr().get())());
-  pointwise_op_f.setArg<at::native::opencl::OpenCLOperations>(3, op);
+  pointwise_op_f.setArg<cl_mem>(0, (*(cl::Buffer*)a->data_ptr().get())());
+  pointwise_op_f.setArg<cl_mem>(1, (*(cl::Buffer*)b->data_ptr().get())());
+  pointwise_op_f.setArg<cl_mem>(2, (*(cl::Buffer*)c->data_ptr().get())());
+  pointwise_op_f.setArg<at::native::opencl::OpenCLOperationsPointwise3>(3, op);
   auto stream = caffe2::opencl::getCurrentOpenCLStream(a->device().index());
   cl::Event event;
   stream.stream()->enqueueNDRangeKernel(pointwise_op_f, 0, a->numel(), cl::NullRange, NULL, &event);
   event.wait();
 }
 
-static void pointwise_op(TensorImpl* b, const TensorImpl* a, at::native::opencl::OpenCLOperations op) {
+static void pointwise_op2s(StorageImpl* c, const StorageImpl* a, const Scalar b, at::native::opencl::OpenCLOperationsPointwise3 op) {
+  // DONE Call OpenCL kernel.
+  auto opt_kernel = c10::opencl::opencl_kernel("pointwise_op_2fv");
+  if (!opt_kernel.has_value()) {
+    TORCH_WARN("No value for kernel \"pointwise_op_2fv\"");
+    return;
+  }
+  cl::Kernel pointwise_op_f = opt_kernel.value();
+  pointwise_op_f.setArg<cl_mem>(0, (*(cl::Buffer*)a->data_ptr().get())());
+  pointwise_op_f.setArg<float>(1, b.toFloat());
+  pointwise_op_f.setArg<cl_mem>(2, (*(cl::Buffer*)c->data_ptr().get())());
+  pointwise_op_f.setArg<at::native::opencl::OpenCLOperationsPointwise3>(3, op);
+  auto stream = caffe2::opencl::getCurrentOpenCLStream(a->device().index());
+  cl::Event event;
+  stream.stream()->enqueueNDRangeKernel(pointwise_op_f, 0, a->numel(), cl::NullRange, NULL, &event);
+  event.wait();
+}
+
+static void pointwise_op(StorageImpl* b, const StorageImpl* a, at::native::opencl::OpenCLOperationsPointwise op) {
   // DONE Call OpenCL kernel.
   auto opt_kernel = c10::opencl::opencl_kernel("pointwise_op_f");
   if (!opt_kernel.has_value()) {
@@ -65,8 +84,9 @@ static void pointwise_op(TensorImpl* b, const TensorImpl* a, at::native::opencl:
     return;
   }
   cl::Kernel pointwise_op_f = opt_kernel.value();
-  pointwise_op_f.setArg<cl_mem>(0, (*(cl::Buffer*)a->storage().unsafeGetStorageImpl()->data_ptr().get())());pointwise_op_f.setArg<cl_mem>(1, (*(cl::Buffer*)b->storage().unsafeGetStorageImpl()->data_ptr().get())());
-  pointwise_op_f.setArg<at::native::opencl::OpenCLOperations>(2, op);
+  pointwise_op_f.setArg<cl_mem>(0, (*(cl::Buffer*)a->data_ptr().get())());
+  pointwise_op_f.setArg<cl_mem>(1, (*(cl::Buffer*)b->data_ptr().get())());
+  pointwise_op_f.setArg<at::native::opencl::OpenCLOperationsPointwise>(2, op);
   auto stream = caffe2::opencl::getCurrentOpenCLStream(a->device().index());
   cl::Event event;
   stream.stream()->enqueueNDRangeKernel(pointwise_op_f, 0, a->numel(), cl::NullRange, NULL, &event);
@@ -74,10 +94,15 @@ static void pointwise_op(TensorImpl* b, const TensorImpl* a, at::native::opencl:
 }
 
 // See THC_logicalTensor in aten/src/THC/THCTensorMathCompareT.cuh for implementation details
-static void logical_tensor(TensorImpl *self_, const TensorImpl *t1, const TensorImpl *t2, at::native::opencl::OpenCLOperations op) {
+static void logical_tensor(TensorImpl *self_, const TensorImpl *t1, const TensorImpl *t2, at::native::opencl::OpenCLOperationsPointwise3 op) {
   opencl_resize(self_, t1->sizes(), {});
   TORCH_CHECK(opencl_nElement(t1) == opencl_nElement(t2), "sizes don't match");
-  pointwise_op3(self_, t1, t2, op);
+  pointwise_op3(self_->storage().unsafeGetStorageImpl(), t1->storage().unsafeGetStorageImpl(), t2->storage().unsafeGetStorageImpl(), op);
+}
+
+static void logical_tensor(TensorImpl *self_, const TensorImpl *t1, const Scalar t2, at::native::opencl::OpenCLOperationsPointwise3 op) {
+  opencl_resize(self_, t1->sizes(), {});
+  pointwise_op2s(self_->storage().unsafeGetStorageImpl(), t1->storage().unsafeGetStorageImpl(), t2, op);
 }
 
 Tensor _eq_opencl(const Tensor &self, const Tensor& other) {
@@ -87,7 +112,7 @@ Tensor _eq_opencl(const Tensor &self, const Tensor& other) {
   auto result = Tensor(c10::intrusive_ptr<TensorImpl, UndefinedTensorImpl>::reclaim(result_));
   auto self_ = checked_tensor_unwrap(self, "self", 1, "_eq_opencl", false, c10::Backend::OpenCL, ScalarType::Float);
   auto other_ = checked_tensor_unwrap(other, "other", 2, "_eq_opencl", false, c10::Backend::OpenCL, ScalarType::Float);
-  logical_tensor(result_, self_, other_, at::native::opencl::OpenCLOperations::EQ);
+  logical_tensor(result_, self_, other_, at::native::opencl::OpenCLOperationsPointwise3::EQ);
   result_->maybe_zero_dim(self_->dim() == 0 && other_->dim() == 0);
   return result;
 }
@@ -96,19 +121,41 @@ Tensor _eq_opencl(const Tensor &self, Scalar other) {
   auto allocator = c10::GetAllocator(DeviceType::OPENCL);
   auto result_ = c10::make_intrusive<TensorImpl, UndefinedTensorImpl>(c10::Storage(scalarTypeToTypeMeta(self.scalar_type()), 0, allocator, true),TensorTypeId::OpenCLTensorId).release();
   auto result = Tensor(c10::intrusive_ptr<TensorImpl, UndefinedTensorImpl>::reclaim(result_));
-  // TODO Perform checks and call logical_tensor with OpenCLOperations::EQ
-  // Check _th_eq from build/aten/src/ATen/LegacyTHFunctionsCUDA.cpp for implementation details.
+  auto self_ = checked_tensor_unwrap(self, "self", 1, "_eq_opencl", false, c10::Backend::OpenCL, ScalarType::Float);
+  logical_tensor(result_, self_, other, at::native::opencl::OpenCLOperationsPointwise3::EQ);
+  result_->maybe_zero_dim(self_->dim() == 0);
   return result;
 }
 
-Tensor _abs_out_opencl(const Tensor &self) {
+Tensor _ne_opencl(const Tensor &self, const Tensor& other) {
+  // TODO Implement this function for every scalar_type
   auto allocator = c10::GetAllocator(DeviceType::OPENCL);
   auto result_ = c10::make_intrusive<TensorImpl, UndefinedTensorImpl>(c10::Storage(scalarTypeToTypeMeta(self.scalar_type()), 0, allocator, true),TensorTypeId::OpenCLTensorId).release();
   auto result = Tensor(c10::intrusive_ptr<TensorImpl, UndefinedTensorImpl>::reclaim(result_));
-  auto self_ = checked_tensor_unwrap(self, "self", 1, "_abs_out_opencl", false, c10::Backend::OpenCL, ScalarType::Float);
+  auto self_ = checked_tensor_unwrap(self, "self", 1, "_ne_opencl", false, c10::Backend::OpenCL, ScalarType::Float);
+  auto other_ = checked_tensor_unwrap(other, "other", 2, "_ne_opencl", false, c10::Backend::OpenCL, ScalarType::Float);
+  logical_tensor(result_, self_, other_, at::native::opencl::OpenCLOperationsPointwise3::NE);
+  result_->maybe_zero_dim(self_->dim() == 0 && other_->dim() == 0);
+  return result;
+}
+
+Tensor _ne_opencl(const Tensor &self, Scalar other) {
+  // TODO Implement this function for every scalar_type
+  auto allocator = c10::GetAllocator(DeviceType::OPENCL);
+  auto result_ = c10::make_intrusive<TensorImpl, UndefinedTensorImpl>(c10::Storage(scalarTypeToTypeMeta(self.scalar_type()), 0, allocator, true),TensorTypeId::OpenCLTensorId).release();
+  auto result = Tensor(c10::intrusive_ptr<TensorImpl, UndefinedTensorImpl>::reclaim(result_));
+  auto self_ = checked_tensor_unwrap(self, "self", 1, "_ne_opencl", false, c10::Backend::OpenCL, ScalarType::Float);
+  logical_tensor(result_, self_, other, at::native::opencl::OpenCLOperationsPointwise3::NE);
+  result_->maybe_zero_dim(self_->dim() == 0);
+  return result;
+}
+
+Tensor & _abs_out_opencl(Tensor &result, const Tensor &self) {
+  auto result_ = checked_tensor_unwrap(result, "result", 1, "_abs_out_opencl", false, c10::Backend::OpenCL, ScalarType::Float);
+  auto self_ = checked_tensor_unwrap(self, "self", 2, "_abs_out_opencl", false, c10::Backend::OpenCL, ScalarType::Float);
 
   opencl_resize(result_, self_->sizes(), {});
-  pointwise_op(result_, self_, at::native::opencl::OpenCLOperations::ABS);
+  pointwise_op(result_->storage().unsafeGetStorageImpl(), self_->storage().unsafeGetStorageImpl(), at::native::opencl::OpenCLOperationsPointwise::ABS);
 
   return result;
 }
