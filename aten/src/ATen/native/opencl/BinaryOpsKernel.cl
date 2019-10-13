1e45ca4973461882c52f66c4e1f6fc474dcc23cf
Benjamin <benjamin.heinen@polymtl.ca>
2019-10-13 13:23:10 -0400

    sub_out, mul_out and div_out finish

--- aten/src/ATen/native/opencl/BinaryOpsKernel.cl
+++ aten/src/ATen/native/opencl/BinaryOpsKernel.cl
@@ -13,15 +13,24 @@ __kernel void pointwise_op3##suffix##_s(__global const type* a, __global const t
             out[get_global_id(0)] = a[get_global_id(0)] - other[get_global_id(0)] * alpha; \
             break; \
         } \
-        case MUL: { \
-            out[get_global_id(0)] = a[get_global_id(0)] * other[get_global_id(0)] * alpha; \
-            break; \
-        } \
-        case DIV: { \
-            out[get_global_id(0)] = a[get_global_id(0)] / other[get_global_id(0)] * alpha; \
-            break; \
-        } \
     } \
 }
 
 DEFINE_KERNEL_FOR_ALL_TYPES(POINTWISE_OP_3S)
+
+#define POINTWISE_OP_3(suffix, type) \
+__kernel void pointwise_op3##suffix##(__global const type* a, __global const type* other, __global type* out, const enum OpenCLOperationsPointwise3s op) { \
+    switch(op) { \
+        case MUL: { \
+            out[get_global_id(0)] = a[get_global_id(0)] * other[get_global_id(0)]; \
+            break; \
+        }\
+        case DIV: { \
+            out[get_global_id(0)] = a[get_global_id(0)] / other[get_global_id(0)]; \
+            break; \
+        } \
+        default: { \
+            break; \
+        } \
+    } \
+}
--- aten/src/ATen/native/opencl/BinaryOpsKernel.cpp
+++ aten/src/ATen/native/opencl/BinaryOpsKernel.cpp
@@ -45,6 +45,25 @@ static void pointwise_op3s(const StorageImpl* a, const StorageImpl* b, StorageIm
   stream.stream()->finish();
 }
 
+
+static void pointwise_op3(const StorageImpl* a, const StorageImpl* b, StorageImpl* out, at::native::opencl::OpenCLOperationsPointwise3 op, const ScalarType scalar_type) {
+  // DONE Call OpenCL kernel.
+  auto kernel_name = "pointwise_op3" + getOpenCLKernelTypeSuffix(scalar_type);
+  auto opt_kernel = c10::opencl::opencl_kernel(kernel_name);
+  if (!opt_kernel) {
+    TORCH_WARN("No value for kernel \"", kernel_name, "\"");
+    return;
+  }
+  cl::Kernel pointwise_op = opt_kernel.value();
+  pointwise_op.setArg<cl_mem>(0, (*(cl::Buffer*)a->data_ptr().get())());
+  pointwise_op.setArg<cl_mem>(1, (*(cl::Buffer*)b->data_ptr().get())());
+  pointwise_op.setArg<cl_mem>(2, (*(cl::Buffer*)out->data_ptr().get())());
+  pointwise_op.setArg<at::native::opencl::OpenCLOperationsPointwise3>(3, op);
+  auto stream = caffe2::opencl::getCurrentOpenCLStream(a->device().index());
+  stream.stream()->enqueueNDRangeKernel(pointwise_op, /*offset=*/0, a->numel(), 1);
+  stream.stream()->finish();
+}
+
 Tensor & opencl_add_out(Tensor &out, const Tensor &self, const Tensor& other , Scalar alpha){
     auto other_ = checked_tensor_unwrap(out, "out", 1, "add_out_opencl", false, c10::Backend::OpenCL, self.scalar_type());
     auto self_ = checked_tensor_unwrap(self, "self", 2, "add_out_opencl", false, c10::Backend::OpenCL, self.scalar_type());
@@ -67,65 +86,45 @@ Tensor & opencl_add_out(Tensor &out, const Tensor &self, const Tensor& other , S
     return out;
 }
 
-// static void sub_kernel_cuda(TensorIterator& iter, Scalar alpha_scalar) {
-//   add_kernel_cuda(iter, -alpha_scalar);
-// }
 
-// void div_kernel_cuda(TensorIterator& iter) {
-//   if (!isIntegralType(iter.dtype(), /*includeBool*/ false) && iter.is_cpu_scalar(2)) {
-//     // optimization for floating-point types: if the second operand is a CPU
-//     // scalar, compute a * reciprocal(b). Note that this may lose one bit of
-//     // precision compared to computing the division.
-//     AT_DISPATCH_FLOATING_TYPES_AND_HALF(iter.dtype(), "div_cuda", [&]() {
-//       auto inv_b = scalar_t(1.0 / iter.scalar_value<scalar_t>(2));
-//       iter.remove_operand(2);
-//       gpu_kernel(iter, [inv_b]GPU_LAMBDA(scalar_t a) -> scalar_t {
-//         return a * inv_b;
-//       });
-//     });
-//   } else {
-//     AT_DISPATCH_ALL_TYPES_AND(kHalf, iter.dtype(), "div_cuda", [&]() {
-//       gpu_kernel_with_scalars(iter, []GPU_LAMBDA(scalar_t a, scalar_t b) -> scalar_t {
-//         return a / b;
-//       });
-//     });
-//   }
-// }
+Tensor & opencl_sub_out(Tensor &out, const Tensor &self, const Tensor& other , Scalar alpha){
+    auto other_ = checked_tensor_unwrap(out, "out", 1, "opencl_sub_out", false, c10::Backend::OpenCL, self.scalar_type());
+    auto self_ = checked_tensor_unwrap(self, "self", 2, "opencl_sub_out", false, c10::Backend::OpenCL, self.scalar_type());
+    auto out_ = checked_tensor_unwrap(out, "out", 3, "opencl_sub_out", false, c10::Backend::OpenCL, self.scalar_type());
+    
+    auto scalar_type = self.scalar_type();
+    switch (scalar_type)
+    {
+#define DEFINE_OPENCL_ADD_CASE(type, name) \
+        case ScalarType::name: \
+            pointwise_op3s<ScalarType::name, type>(self_->storage().unsafeGetStorageImpl(), other_->storage().unsafeGetStorageImpl(), out_->storage().unsafeGetStorageImpl(), alpha, at::native::opencl::OpenCLOperationsPointwise3s::SUB); \
+            break;
+        AT_FORALL_SCALAR_TYPES_WITH_COMPLEX_AND_QINTS(DEFINE_OPENCL_ADD_CASE)
+#undef DEFINE_OPENCL_ADD_CASE
 
-// void mul_kernel_cuda(TensorIterator& iter) {
-//   if (iter.dtype() == ScalarType::Bool) {
-//     // Workaround for the error: '*' in boolean context, suggest '&&' instead [-Werror=int-in-bool-context]
-//     gpu_kernel_with_scalars(iter, []GPU_LAMBDA(bool a, bool b) -> bool {
-//       return a && b;
-//     });
-//   } else {
-//     AT_DISPATCH_ALL_TYPES_AND(kHalf, iter.dtype(), "mul_cuda", [&]() {
-//       gpu_kernel_with_scalars(iter, []GPU_LAMBDA(scalar_t a, scalar_t b) -> scalar_t {
-//         return a * b;
-//       });
-//     });
-//   }
-// }
+    default:
+      TORCH_CHECK(false, "logical_tensor not supported on OpenCLType for ", scalar_type);
+      break;
+    }
+    return out;
+}
 
-// void atan2_kernel_cuda(TensorIterator& iter) {
-//   AT_DISPATCH_FLOATING_TYPES_AND_HALF(iter.dtype(), "atan2_cuda", [&]() {
-//     gpu_kernel_with_scalars(iter, []GPU_LAMBDA(scalar_t a, scalar_t b) -> scalar_t {
-//       return THCNumerics<scalar_t>::atan2(a, b);
-//     });
-//   });
-// }
+Tensor & opencl_mul_out(Tensor &out, const Tensor &self, const Tensor& other){
+    auto other_ = checked_tensor_unwrap(out, "out", 1, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
+    auto self_ = checked_tensor_unwrap(self, "self", 2, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
+    auto out_ = checked_tensor_unwrap(out, "out", 3, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
+    
+    pointwise_op3(self_->storage().unsafeGetStorageImpl(), other_->storage().unsafeGetStorageImpl(), out_->storage().unsafeGetStorageImpl(), at::native::opencl::OpenCLOperationsPointwise3::MUL, self.scalar_type());
+    return out;
+}
 
-// void logical_xor_kernel_cuda(TensorIterator& iter) {
-//   gpu_kernel(iter, []GPU_LAMBDA(bool a, bool b) -> bool {
-//     return a != b;
-//   });
-// }
-
-//REGISTER_OPENCL_DISPATCH(add_stub, &add_out);
-// REGISTER_OPENCL_DISPATCH(sub_stub, &sub_kernel_cuda);
-// REGISTER_OPENCL_DISPATCH(div_stub, &div_kernel_cuda);
-// REGISTER_OPENCL_DISPATCH(mul_stub, &mul_kernel_cuda);
-// REGISTER_OPENCL_DISPATCH(atan2_stub, &atan2_kernel_cuda);
-// REGISTER_OPENCL_DISPATCH(logical_xor_stub, &logical_xor_kernel_cuda);
+Tensor & opencl_div_out(Tensor &out, const Tensor &self, const Tensor& other){
+    auto other_ = checked_tensor_unwrap(out, "out", 1, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
+    auto self_ = checked_tensor_unwrap(self, "self", 2, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
+    auto out_ = checked_tensor_unwrap(out, "out", 3, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
+    
+    pointwise_op3(self_->storage().unsafeGetStorageImpl(), other_->storage().unsafeGetStorageImpl(), out_->storage().unsafeGetStorageImpl(), at::native::opencl::OpenCLOperationsPointwise3::DIV, self.scalar_type());
+    return out;
+}
 
 }} // namespace at::native
