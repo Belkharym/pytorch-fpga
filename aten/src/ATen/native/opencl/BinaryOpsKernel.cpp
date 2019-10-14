0bb9c0578176696b3688af0c7c467c73401f7e7d
Benjamin <benjamin.heinen@polymtl.ca>
2019-10-14 01:19:29 -0400

    dispatch with dispatcher on each case

--- aten/src/ATen/native/opencl/BinaryOpsKernel.cpp
+++ aten/src/ATen/native/opencl/BinaryOpsKernel.cpp
@@ -67,115 +67,115 @@ static void pointwise_op3(const StorageImpl* a, const StorageImpl* b, StorageImp
 
 // ADD FUNCTION
 
-Tensor & opencl_add_out(Tensor &out, const Tensor &self, const Tensor& other , Scalar alpha){
-    auto other_ = checked_tensor_unwrap(other, "other", 1, "add_out_opencl", false, c10::Backend::OpenCL, self.scalar_type());
-    auto self_ = checked_tensor_unwrap(self, "self", 2, "add_out_opencl", false, c10::Backend::OpenCL, self.scalar_type());
-    auto out_ = checked_tensor_unwrap(out, "out", 3, "add_out_opencl", false, c10::Backend::OpenCL, self.scalar_type());
+// Tensor & opencl_add_out(Tensor &out, const Tensor &self, const Tensor& other , Scalar alpha){
+//     auto other_ = checked_tensor_unwrap(other, "other", 1, "add_out_opencl", false, c10::Backend::OpenCL, self.scalar_type());
+//     auto self_ = checked_tensor_unwrap(self, "self", 2, "add_out_opencl", false, c10::Backend::OpenCL, self.scalar_type());
+//     auto out_ = checked_tensor_unwrap(out, "out", 3, "add_out_opencl", false, c10::Backend::OpenCL, self.scalar_type());
     
-    auto scalar_type = self.scalar_type();
-    switch (scalar_type)
-    {
-#define DEFINE_OPENCL_ADD_CASE(type, name) \
-        case ScalarType::name: \
-            pointwise_op3s<ScalarType::name, type>(self_->storage().unsafeGetStorageImpl(), other_->storage().unsafeGetStorageImpl(), out_->storage().unsafeGetStorageImpl(), alpha, at::native::opencl::OpenCLOperationsPointwise3s::ADD); \
-            break;
-        AT_FORALL_SCALAR_TYPES_WITH_COMPLEX_AND_QINTS(DEFINE_OPENCL_ADD_CASE)
-#undef DEFINE_OPENCL_ADD_CASE
+//     auto scalar_type = self.scalar_type();
+//     switch (scalar_type)
+//     {
+// #define DEFINE_OPENCL_ADD_CASE(type, name) \
+//         case ScalarType::name: \
+//             pointwise_op3s<ScalarType::name, type>(self_->storage().unsafeGetStorageImpl(), other_->storage().unsafeGetStorageImpl(), out_->storage().unsafeGetStorageImpl(), alpha, at::native::opencl::OpenCLOperationsPointwise3s::ADD); \
+//             break;
+//         AT_FORALL_SCALAR_TYPES_WITH_COMPLEX_AND_QINTS(DEFINE_OPENCL_ADD_CASE)
+// #undef DEFINE_OPENCL_ADD_CASE
 
-    default:
-      TORCH_CHECK(false, "logical_tensor not supported on OpenCLType for ", scalar_type);
-      break;
-    }
-    return out;
-}
+//     default:
+//       TORCH_CHECK(false, "logical_tensor not supported on OpenCLType for ", scalar_type);
+//       break;
+//     }
+//     return out;
+// }
 
-Tensor & opencl_add_(Tensor &self, const Tensor& other , Scalar alpha){
-    return opencl_add_out(self, self, other, alpha);
-}
+// Tensor & opencl_add_(Tensor &self, const Tensor& other , Scalar alpha){
+//     return opencl_add_out(self, self, other, alpha);
+// }
 
-Tensor opencl_add(const Tensor &self, const Tensor& other , Scalar alpha){
-    Tensor out =  at::native::empty_opencl(self.sizes(), self.options());
-    opencl_add_out(out, self, other, alpha);
-    return out;
-}
+// Tensor opencl_add(const Tensor &self, const Tensor& other , Scalar alpha){
+//     Tensor out =  at::native::empty_opencl(self.sizes(), self.options());
+//     opencl_add_out(out, self, other, alpha);
+//     return out;
+// }
 
-// SUB FUNCTION
+// // SUB FUNCTION
 
-Tensor & opencl_sub_out(Tensor &out, const Tensor &self, const Tensor& other , Scalar alpha){
-    auto other_ = checked_tensor_unwrap(other, "other", 1, "opencl_sub_out", false, c10::Backend::OpenCL, self.scalar_type());
-    auto self_ = checked_tensor_unwrap(self, "self", 2, "opencl_sub_out", false, c10::Backend::OpenCL, self.scalar_type());
-    auto out_ = checked_tensor_unwrap(out, "out", 3, "opencl_sub_out", false, c10::Backend::OpenCL, self.scalar_type());
+// Tensor & opencl_sub_out(Tensor &out, const Tensor &self, const Tensor& other , Scalar alpha){
+//     auto other_ = checked_tensor_unwrap(other, "other", 1, "opencl_sub_out", false, c10::Backend::OpenCL, self.scalar_type());
+//     auto self_ = checked_tensor_unwrap(self, "self", 2, "opencl_sub_out", false, c10::Backend::OpenCL, self.scalar_type());
+//     auto out_ = checked_tensor_unwrap(out, "out", 3, "opencl_sub_out", false, c10::Backend::OpenCL, self.scalar_type());
     
-    auto scalar_type = self.scalar_type();
-    switch (scalar_type)
-    {
-#define DEFINE_OPENCL_ADD_CASE(type, name) \
-        case ScalarType::name: \
-            pointwise_op3s<ScalarType::name, type>(self_->storage().unsafeGetStorageImpl(), other_->storage().unsafeGetStorageImpl(), out_->storage().unsafeGetStorageImpl(), alpha, at::native::opencl::OpenCLOperationsPointwise3s::SUB); \
-            break;
-        AT_FORALL_SCALAR_TYPES_WITH_COMPLEX_AND_QINTS(DEFINE_OPENCL_ADD_CASE)
-#undef DEFINE_OPENCL_ADD_CASE
+//     auto scalar_type = self.scalar_type();
+//     switch (scalar_type)
+//     {
+// #define DEFINE_OPENCL_ADD_CASE(type, name) \
+//         case ScalarType::name: \
+//             pointwise_op3s<ScalarType::name, type>(self_->storage().unsafeGetStorageImpl(), other_->storage().unsafeGetStorageImpl(), out_->storage().unsafeGetStorageImpl(), alpha, at::native::opencl::OpenCLOperationsPointwise3s::SUB); \
+//             break;
+//         AT_FORALL_SCALAR_TYPES_WITH_COMPLEX_AND_QINTS(DEFINE_OPENCL_ADD_CASE)
+// #undef DEFINE_OPENCL_ADD_CASE
 
-    default:
-      TORCH_CHECK(false, "logical_tensor not supported on OpenCLType for ", scalar_type);
-      break;
-    }
-    return out;
-}
+//     default:
+//       TORCH_CHECK(false, "logical_tensor not supported on OpenCLType for ", scalar_type);
+//       break;
+//     }
+//     return out;
+// }
 
-Tensor & opencl_sub_(Tensor &self, const Tensor& other , Scalar alpha){
-    return opencl_sub_out(self, self, other, alpha);
-}
+// Tensor & opencl_sub_(Tensor &self, const Tensor& other , Scalar alpha){
+//     return opencl_sub_out(self, self, other, alpha);
+// }
 
-Tensor opencl_sub(const Tensor &self, const Tensor& other , Scalar alpha){
-    Tensor out =  at::native::empty_opencl(self.sizes(), self.options());
-    opencl_sub_out(out, self, other, alpha);
-    return out;
-}
+// Tensor opencl_sub(const Tensor &self, const Tensor& other , Scalar alpha){
+//     Tensor out =  at::native::empty_opencl(self.sizes(), self.options());
+//     opencl_sub_out(out, self, other, alpha);
+//     return out;
+// }
 
-// MUL FUNCTION
+// // MUL FUNCTION
 
 
-Tensor & opencl_mul_out(Tensor &out, const Tensor &self, const Tensor& other){
-    auto other_ = checked_tensor_unwrap(other, "other", 1, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
-    auto self_ = checked_tensor_unwrap(self, "self", 2, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
-    auto out_ = checked_tensor_unwrap(out, "out", 3, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
+// Tensor & opencl_mul_out(Tensor &out, const Tensor &self, const Tensor& other){
+//     auto other_ = checked_tensor_unwrap(other, "other", 1, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
+//     auto self_ = checked_tensor_unwrap(self, "self", 2, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
+//     auto out_ = checked_tensor_unwrap(out, "out", 3, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
     
-    pointwise_op3(self_->storage().unsafeGetStorageImpl(), other_->storage().unsafeGetStorageImpl(), out_->storage().unsafeGetStorageImpl(), at::native::opencl::OpenCLOperationsPointwise3::MUL, self.scalar_type());
-    return out;
-}
+//     pointwise_op3(self_->storage().unsafeGetStorageImpl(), other_->storage().unsafeGetStorageImpl(), out_->storage().unsafeGetStorageImpl(), at::native::opencl::OpenCLOperationsPointwise3::MUL, self.scalar_type());
+//     return out;
+// }
 
-Tensor & opencl_mul_(Tensor &self, const Tensor& other){
-    return opencl_mul_out(self, self, other);
-}
+// Tensor & opencl_mul_(Tensor &self, const Tensor& other){
+//     return opencl_mul_out(self, self, other);
+// }
 
-Tensor opencl_mul(const Tensor &self, const Tensor& other){
-    Tensor out =  at::native::empty_opencl(self.sizes(), self.options());
-    opencl_mul_out(out, self, other);
-    return out;
-}
+// Tensor opencl_mul(const Tensor &self, const Tensor& other){
+//     Tensor out =  at::native::empty_opencl(self.sizes(), self.options());
+//     opencl_mul_out(out, self, other);
+//     return out;
+// }
 
-// DIV FUNCTION
+// // DIV FUNCTION
 
 
-Tensor & opencl_div_out(Tensor &out, const Tensor &self, const Tensor& other){
-    auto other_ = checked_tensor_unwrap(other, "other", 1, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
-    auto self_ = checked_tensor_unwrap(self, "self", 2, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
-    auto out_ = checked_tensor_unwrap(out, "out", 3, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
+// Tensor & opencl_div_out(Tensor &out, const Tensor &self, const Tensor& other){
+//     auto other_ = checked_tensor_unwrap(other, "other", 1, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
+//     auto self_ = checked_tensor_unwrap(self, "self", 2, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
+//     auto out_ = checked_tensor_unwrap(out, "out", 3, "opencl_mul_out", false, c10::Backend::OpenCL, self.scalar_type());
     
-    pointwise_op3(self_->storage().unsafeGetStorageImpl(), other_->storage().unsafeGetStorageImpl(), out_->storage().unsafeGetStorageImpl(), at::native::opencl::OpenCLOperationsPointwise3::DIV, self.scalar_type());
-    return out;
-}
+//     pointwise_op3(self_->storage().unsafeGetStorageImpl(), other_->storage().unsafeGetStorageImpl(), out_->storage().unsafeGetStorageImpl(), at::native::opencl::OpenCLOperationsPointwise3::DIV, self.scalar_type());
+//     return out;
+// }
 
-Tensor & opencl_div_(Tensor &self, const Tensor& other){
-    return opencl_div_out(self, self, other);
-}
+// Tensor & opencl_div_(Tensor &self, const Tensor& other){
+//     return opencl_div_out(self, self, other);
+// }
 
-Tensor opencl_div(const Tensor &self, const Tensor& other){
-    Tensor out =  at::native::empty_opencl(self.sizes(), self.options());
-    opencl_div_out(out, self, other);
-    return out;
-}
+// Tensor opencl_div(const Tensor &self, const Tensor& other){
+//     Tensor out =  at::native::empty_opencl(self.sizes(), self.options());
+//     opencl_div_out(out, self, other);
+//     return out;
+// }
 
 
 
