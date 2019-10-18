9360f2784a4a0e9b919757bc478f324bb8cbd9a5
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2019-10-18 00:47:54 -0400

    opencl: improved AWSFPGA toolchain

--- cmake/toolchains/AWSFPGA.cmake
+++ cmake/toolchains/AWSFPGA.cmake
@@ -1,10 +1,11 @@
 
-if (USE_FPGA)
-    find_package(SDAccel REQUIRED)
-    if (NOT SDACCEL_FOUND)
-        MESSAGE(FATAL_ERROR
-            "Could not find SDAccel installation")
-    endif()
-    set(CMAKE_C_COMPILER ${SDACCEL_XCPP})
-    set(CMAKE_CXX_COMPILER ${SDACCEL_XCPP})
+set(USE_CUDA 0)
+set(USE_OPENCL ON)
+set(USE_FPGA ON)
+find_package(SDAccel REQUIRED)
+if (NOT SDACCEL_FOUND)
+    MESSAGE(FATAL_ERROR
+        "Could not find SDAccel installation")
 endif()
+set(CMAKE_C_COMPILER ${SDACCEL_XCPP})
+set(CMAKE_CXX_COMPILER ${SDACCEL_XCPP})
