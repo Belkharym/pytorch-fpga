cceaf679f19204c72cb6c22320353c436cc775fb
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2019-10-25 16:58:45 -0400

    opencl: fixed support for windows

--- aten/src/ATen/opencl/ATenOpenCLGeneral.h
+++ aten/src/ATen/opencl/ATenOpenCLGeneral.h
@@ -1,13 +1,6 @@
 #pragma once
 
-#include <c10/macros/Export.h>
+#include <c10/macros/Macros.h>
 #include <c10/opencl/OpenCLMacros.h>
 
 #define AT_OPENCL_API CAFFE2_API
-
-namespace at {
-namespace opencl {
-
-using namespace c10::opencl;
-
-}} // at::opencl
